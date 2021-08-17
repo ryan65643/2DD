e91b3c2e66804115a048240a352f5df4815cadbd
ryan65643 <57181200+ryan65643@users.noreply.github.com>
2021-08-17 14:00:17 +0800

    2

---
+++ 2DD/Assets/Script/Meau.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Meau : MonoBehaviour
+{
+    public GameObject StartGame;
+
+    private void Awake()
+    {
+        StartGame = GameObject.Find("Canvas");
+    }
+
+    public void Play()
+    {
+      StartGame.SetActive(false);
+    }
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
